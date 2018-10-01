7c2ed2dc7a21679a5849276f66fe5b1f8703ac4b
rebfarish <rebfarish@gmail.com>
2018-10-01 10:56:09 -0600

    rewrote shuffle method and added different primitive data types.

--- src/edu/cnm/deepdive/Shuffler.java
+++ src/edu/cnm/deepdive/Shuffler.java
@@ -9,26 +9,130 @@ public class Shuffler {
 
   public static void main(String[] args) {
 
-    Integer[] deck = new Integer[52];
+    int[] deck = new int[52];
 
-    for (int i = 0; i < deck.length; i++){
-      deck[i] = i +1;
+    for (int i = 0; i < deck.length; i++) {
+      deck[i] = i + 1;
     }
-System.out.println(Arrays.toString(deck));
+    System.out.println(Arrays.toString(deck));
     shuffle(deck);
     System.out.println(Arrays.toString(deck));
   }
 
-  private static void shuffle(Integer[] deck) {
-    Random rng = new Random();
-    Integer [] newDeck = new Integer[52];
-    ArrayList <Integer> deckList = new ArrayList<Integer>(Arrays.asList(deck));
-    for (int i = 0; i < deck.length; i++) {
-      newDeck[i] = deckList.remove(rng.nextInt(deckList.size()));
-    }
-
-    System.arraycopy(newDeck, 0, deck, 0, deck.length);
+  public static void shuffle(int[] deck) {
+    shuffle(deck, new Random());
   }
 
+  public static void shuffle(int[] deck, Random rng) {
+    for (int i = deck.length - 1; i > 0; i--) {
+      int swap = rng.nextInt(i + 1);
+      int temp = deck[swap];
+      deck[swap] = deck[i];
+      deck[i] = temp;
+    }
+  }
 
+  public static void shuffle(byte[] deck, Random rng) {
+    for (int i = deck.length - 1; i > 0; i--) {
+      int swap = rng.nextInt(i + 1);
+      byte temp = deck[swap];
+      deck[swap] = deck[i];
+      deck[i] = temp;
+    }
+  }
+
+  public static void shuffle(byte[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  public static void shuffle(char[] deck, Random rng) {
+    for (int i = deck.length - 1; i > 0; i--) {
+      int swap = rng.nextInt(i + 1);
+      char temp = deck[swap];
+      deck[swap] = deck[i];
+      deck[i] = temp;
+    }
+  }
+
+  public static void shuffle(char[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  public static void shuffle(double[] deck, Random rng) {
+    for (int i = deck.length - 1; i > 0; i--) {
+      int swap = rng.nextInt(i + 1);
+      double temp = deck[swap];
+      deck[swap] = deck[i];
+      deck[i] = temp;
+    }
+  }
+
+  public static void shuffle(double[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  public static void shuffle(float[] deck, Random rng) {
+    for (int i = deck.length - 1; i > 0; i--) {
+      int swap = rng.nextInt(i + 1);
+      float temp = deck[swap];
+      deck[swap] = deck[i];
+      deck[i] = temp;
+    }
+  }
+
+  public static void shuffle(float[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  public static void shuffle(long[] deck, Random rng) {
+    for (int i = deck.length - 1; i > 0; i--) {
+      int swap = rng.nextInt(i + 1);
+      long temp = deck[swap];
+      deck[swap] = deck[i];
+      deck[i] = temp;
+    }
+  }
+
+  public static void shuffle(long[] deck) {
+    shuffle(deck, new Random());
+  }
+
+  public static void shuffle(short[] deck, Random rng) {
+    for (int i = deck.length - 1; i > 0; i--) {
+      int swap = rng.nextInt(i + 1);
+      short temp = deck[swap];
+      deck[swap] = deck[i];
+      deck[i] = temp;
+    }
+  }
+
+  public static void shuffle(short[] deck) {
+    shuffle(deck, new Random());
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
